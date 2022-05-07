f8147cf09e5906d0eb7a88ebcbd3aa4ad839ab3e
William Dias <will.kof1@gmail.com>
2022-05-07 20:01:15 -0300

    add news packages

--- installsubltext.sh
+++ installsubltext.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 ####sh com os comandos para instalar o sublimetext debian e derivados####
 ####fonte dos comandos https://www.sublimetext.com/docs/3/linux_repositories.html######
-apt update && apt upgrade -y
-echo "Install the GPG key"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "install dependencias"
-sudo apt-get install apt-transport-https
-echo "Select the channel to use: Stable"
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-echo "update and install"
-sudo apt-get update
-sudo apt-get install sublime-text -y
-sudo apt-get install terraform -y
-sudo apt-get install spotify-client -y
-sudo apt install keepass2 -y
+sudo apt install apt-transport-https gnupg software-properties-common curl wget gpg ca-certificates lsb-release software-properties-common -y
+apt update && apt upgrade -y
+echo 'deb [signed-by=/usr/share/keyrings/sublimehq.gpg] https://download.sublimetext.com/ apt/stable/' | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update && sudo apt install sublime-text -y
+echo "add repo terraform"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update && sudo apt install terraform -y
+echo "add repo spotify"
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update && sudo apt install spotify-client -y
 sudo apt install awscli -y
 sudo apt install flameshot -y
 sudo apt install mysql-client -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update && sudo apt install code # or code-insiders
+rm -f packages.microsoft.gpg wget-log
