f5c606ff3cff3e745a880632f6fb77eb8fc93caa
William Dias <will.kof1@gmail.com>
2023-12-09 17:07:21 -0300

    adiciona echo

--- update_discord.sh
+++ update_discord.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-wget -O ~/Downloads/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-sudo dpkg -i ~/Downloads/discord.deb
+echo "Inciando Download da nova versao"
+wget -O /home/william/Downloads/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+echo "Iniciando a instalacao"
+sudo dpkg -i /home/william/Downloads/discord.deb
+echo "Remove pra nao ficar sujeira"
+sudo rm -rf /home/william/Downloads/discord.deb
+echo "Listar pra confirmar"
+sudo ls -l /home/william/Downloads/discord.deb
