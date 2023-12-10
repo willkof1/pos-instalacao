ca4d36b8f6fb5cc1baeb6c19170b35783d477649
Will D <will.kof1@gmail.com>
2023-12-10 12:20:38 -0300

    fix update_discord.sh

--- update_discord.sh
+++ update_discord.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "Inciando Download da nova versao"
-wget -O /home/william/Downloads/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+echo ""
+echo -e "\e[1;43m Inciando Download da nova versao! \e[0m"
+echo ""
+wget -O /tmp/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 echo "Iniciando a instalacao"
-sudo dpkg -i /home/william/Downloads/discord.deb
+echo
+sudo dpkg -i /tmp/discord.deb
 echo "Remove pra nao ficar sujeira"
-sudo rm -rf /home/william/Downloads/discord.deb
-echo "Listar pra confirmar"
-sudo ls -l /home/william/Downloads/discord.deb
+sleep 10
+sudo rm -rf /tmp/discord.deb
+echo ""
+echo -e "\e[1;43m Foi removido!! \e[0m"
